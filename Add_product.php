f78e52342309d41ee9bda8ecfa375ab58c19dcbc
md213229 <118235258+md213229@users.noreply.github.com>
2022-12-06 11:11:24 +0800

    fix: add product page wasnt submitting

--- Add_product.php
+++ Add_product.php
@@ -1,23 +1,24 @@
-<?php
-
-require_once("dbinfo.php");
-
-$mysqli = new mysqli($hostname,$dbUser,$dbPassword,$db); 
-
-// Check connection
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-};
-
-if(isset($_POST['pname'])){
-    $sql = "INSERT INTO product_info(name,price,quantity) VALUES ('$_POST[pname]','$_POST[price]','$_POST[quantity]')";
-    $result = $mysqli->query($sql);
-    if($result){
-        $last_id = $mysqli->insert_id;
-}
-header('Location: displayProducts.php');
-
-}
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Add Product</h2>
+    <form action="Add_product.php" method='post'>
+        <label for="pname">Product Name:</label>
+        <input type="text" id='pname' name='pname'>
+        <br>
+        <label for="product-name">Price:</label>
+        <input type="number" id='price' name='price'>
+        <br>
+        <label for="product-name">Quantity:</label>
+        <input type="number" id='quantity' name='quantity'>
+        <br>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
