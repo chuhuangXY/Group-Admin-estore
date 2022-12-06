412b1df22d7f6dad0b6f4654a8c545679a4964f5
md213229 <118235258+md213229@users.noreply.github.com>
2022-12-06 10:40:26 +0800

    style: Improved table layout

--- displayProducts.php
+++ displayProducts.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
     <table>
@@ -35,7 +36,7 @@
                     echo"
                     <tr>
                     <td>$name</td>
-                    <td>$price</td>
+                    <td>$$price</td>
                     <td>$quantity</td>
                     </tr>
 
